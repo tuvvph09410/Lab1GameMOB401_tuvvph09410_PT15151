80154906267cb5d0c918f02d588c67a7a3afd844
tuvvph09410 <56832432+tuvvph09410@users.noreply.github.com>
2020-07-09 08:28:10 +0700

    Initial commit

---
+++ Assets/Scripts/PlayerContent.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerContent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float speed = 0.5f; // tốc độ
+    private CharacterController controller; // đều phối
+    private float verticalVelocity = 0.0f; // Vận Tốc Ngang
+    private float gravity = 11.0f; // trọng lượng
+    private float animationDuration = 3.0f;
+
+    Vector3 vector3;
+    void Start()
+    {
+
+
+        controller = GetComponent<CharacterController>(); // ánh xạ
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vector3 = Vector3.zero; // khởi tạo
+
+
+        if (Time.time < animationDuration)
+        {
+            // di chuyển 
+            controller.Move(Vector3.forward * speed * Time.deltaTime);
+            return;
+        }
+        if (controller.isGrounded) // nếu là ground
+        {
+            verticalVelocity = -0.5f;
+
+        }
+        else // không phải là ground thì trọng lượng hút xuống
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+        // định nghĩa di chuyển
+        vector3.x = Input.GetAxisRaw("Horizontal") * speed;
+        vector3.y = verticalVelocity;
+        vector3.z = speed;
+        controller.Move(vector3 * Time.deltaTime);
+    }
+}
